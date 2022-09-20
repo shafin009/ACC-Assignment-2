72b4a3254261992ca3792800d75a8b441eb263e4
shafin009 <ghostboyshafin@gmail.com>
2022-09-20 21:58:57 +0600

    'final-done'

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +8,15 @@ const app = require('./app')
 
 
 
-mongoose.connect(process.env.DATABASE).then(() => {
-    console.log('data is on')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@tours-assignment.kwgjtmd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
+
+
 
 app.listen(port, () => {
     console.log('server is running', port);
