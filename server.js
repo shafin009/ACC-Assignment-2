fac3a4fd45ebd4e069ac1b1f9832152a07b68891
shafin009 <ghostboyshafin@gmail.com>
2022-09-20 20:15:13 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+
+
+const app = require('./app')
+
+
+
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+    console.log('data is on')
+});
+
+app.listen(PORT, () => {
+    console.log('server is running', PORT);
+})
